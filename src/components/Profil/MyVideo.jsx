eef378395e5490b6acf03f5248d0eb25bf3d5cd3
PRFE 15 <prfe15@assofac.org>
2024-06-18 00:24:29 +0200

    fix-bug

--- src/components/Profil/MyVideo.jsx
+++ src/components/Profil/MyVideo.jsx
@@ -3,7 +3,7 @@ import MusiqueVideo from '../../assets/Never.mp4';
 
 function MyVideo() {
   return (
-    <div className="video-container">
+    <div className="video-container-profil">
       <div className="background-video-profil">Video</div>
       <video controls muted={false}>
         <source src={MusiqueVideo} type="video/mp4" />
--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -4,6 +4,7 @@ import Footer from '../../components/Basics/Footer'
 import NewsletterForm from '../../components/Accueil/NewsletterForm'
 import Banner from '../../components/Accueil/Banner'
 import Images from '../../components/Accueil/Images'
+
 import '../../styles/Accueil.css';
 import '../../styles/Basics/Footer.css';
 import '../../styles/Basics/Navbar.css';
